df3a598de920c5ae773091d72233b2d5114f408e
daliayan <daliayan5@gmail.com>
2021-05-21 14:18:39 -0400

    Active Record Validations - validates/validate/length/inclusion/min+max/uniqueness/presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 10}
+    validates :summary, length: {maximum: 10}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbait?
+
+    CLICKBAIT = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+    ]
+
+  def is_clickbait?
+    if CLICKBAIT.none? { |pattern| pattern.match title }
+      errors.add(:title, "This is clickbait")
+    end
+  end
+
 end
